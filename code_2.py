e8ae0eea28f7b1149002a0f3ab5ec89ea589163a
Polina <p9991820@gmail.com>
2023-12-07 06:01:06 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,27 @@
+        print(f"Random Number: {item}")
+
+
+    return data
+import random
+    data = generate_random_data()
+
+    main()
+
+if __name__ == "__main__":
+def main():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():def generate_random_data():
+    main()
+    return data
+    for item in data:
+
+    data = generate_random_data()
+
+
+def main():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+        print(f"Random Number: {item}")
