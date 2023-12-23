f03cb64ebfe4a00517dcef676a6bc8673b8da5df
Polina <p9991820@gmail.com>
2023-12-24 01:04:22 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,72 @@
-import random
+    for (let i = 0; i < 10; i++) {
+    return data;
+        console.log("Random Number: " + item);
+}
+    const data = [];
+    const data = generateRandomData();
+    });
+    data.forEach(item => {
+    }
+
+
+main();
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+function generateRandomData() {
+function main() {    const data = generateRandomData();
+    });
+    data.forEach(item => {
+
+}
+    }
+function generateRandomData() {
+main();
+
+    return data;
+        console.log("Random Number: " + item);
+}
+function main() {
+
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+        print(f"Random Number: {item}")
+    for item in data:
+def generate_random_data():
+
 if __name__ == "__main__":
 
-        print(f"Random Number: {item}")
-def main():
 
     data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
+import random
     main()
     data = generate_random_data()
+    return data
+def main():    data = generate_random_data()
 
+import random
+    return data
     for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+def generate_random_data():
+    main()
+        print(f"Random Number: {item}")
+def main():
+if __name__ == "__main__":        print(f"Random Number: {item}")
+
+def generate_random_data():
+if __name__ == "__main__":
+import random
 
     return data
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+    main()
+def main():
